154a98a82aae25c2c77565af9fed50dde32ae65d
raza <razakhanha05@gmail.com>
2023-11-23 19:23:59 +0530

    Added Category(_slug_) Page

---
+++ app/category/[slug]/page.jsx
@@ -0,0 +1,39 @@
+import ProductCard from "@/components/ProductCard";
+import Wrapper from "@/components/Wrapper";
+import React from "react";
+
+const Category = () => {
+  return (
+    <div className="w-full md:py-20 relative">
+      <Wrapper>
+        <div className="text-center max-w-[800px] mx-auto mt-8 md:mt-0">
+          <div className="text-[28px] md:text-[34px] mb-5 font-semibold leading-tight">
+            Running Shoes
+          </div>
+
+          {/* products grid */}
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 my-14 px-5 md:px-0">
+            {/* {products?.data?.map((product) => (? */}
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            {/* ))} */}
+          </div>
+        </div>
+      </Wrapper>
+    </div>
+  );
+};
+
+export default Category;
