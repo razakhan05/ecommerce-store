daba824c60b216ac65a38c8d49f27aab0227accb
raza <razakhanha05@gmail.com>
2023-11-25 11:46:31 +0530

    optimized cartlice added button and input component shadcn

--- store/cartSlice.js
+++ store/cartSlice.js
@@ -1,21 +1,26 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+// Retrieve cart items from localStorage if available
+const initialState = {
+  cartItems: JSON.parse(localStorage?.getItem("cartItems")) || [],
+};
+
 export const cartSlice = createSlice({
   name: "cart",
-  initialState: {
-    cartItems: [],
-  },
+  initialState,
   reducers: {
     addToCart: (state, action) => {
       const totalItems = state.cartItems.find(
         (p) => p.id === action.payload.id
       );
       if (totalItems) {
-        totalItems.qauntity++;
-        totalItems.price = totalItems.singleProductPrice * totalItems.qauntity;
+        totalItems.quantity++;
+        totalItems.price = totalItems.singleProductPrice * totalItems.quantity;
       } else {
-        state.cartItems.push({ ...action.payload, qauntity: 1 });
+        state.cartItems.push({ ...action.payload, quantity: 1 });
       }
+      // Save cart items to localStorage after updating
+      localStorage.setItem("cartItems", JSON.stringify(state.cartItems));
     },
     updateCart: (state, action) => {
       state.cartItems = state.cartItems.map((p) => {
@@ -27,11 +32,15 @@ export const cartSlice = createSlice({
         }
         return p;
       });
+      // Save cart items to localStorage after updating
+      localStorage.setItem("cartItems", JSON.stringify(state.cartItems));
     },
     removeFromCart: (state, action) => {
       state.cartItems = state.cartItems.filter(
         (p) => p.id !== action.payload.id
       );
+      // Save cart items to localStorage after updating
+      localStorage.setItem("cartItems", JSON.stringify(state.cartItems));
     },
   },
 });
